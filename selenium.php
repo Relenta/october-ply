f4543e9ed726a75a3e91acbf1df36a4ede112917
root <root@relenta.dev>
2017-04-04 23:50:10 -0400

    Added selenium.php for selenium tests

---
+++ selenium.php
@@ -0,0 +1,17 @@
+<?php
+
+// Selenium server details
+define('TEST_SELENIUM_HOST', '127.0.0.1');
+define('TEST_SELENIUM_PORT', 4444);
+define('TEST_SELENIUM_BROWSER', '*firefox');
+
+// Back-end URL
+define('TEST_SELENIUM_URL', 'http://127.0.0.1:4444/backend');
+
+// Active Theme
+define('TEST_SELENIUM_THEME', 'demo');
+
+// Back-end credentials
+define('TEST_SELENIUM_USER', 'admin');
+define('TEST_SELENIUM_PASS', 'admin');
+
