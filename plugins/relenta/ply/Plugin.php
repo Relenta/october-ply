80d9b32ce960acaae756b435fcba2ba07c016da3
Artem Egorov <Kartpick@gmail.com>
2017-04-13 12:09:35 +0300

    * added plugin Details (not shown whithout them) * registered components

--- plugins/relenta/ply/Plugin.php
+++ plugins/relenta/ply/Plugin.php
@@ -6,10 +6,25 @@ class Plugin extends PluginBase
 {
     public function registerComponents()
     {
-        return [];
+        return [
+            '\Relenta\Ply\Components\Categories' => 'categories',
+            '\Relenta\Ply\Components\Courses' => 'courses',
+            '\Relenta\Ply\Components\Units' => 'units',
+            '\Relenta\Ply\Components\Cards' => 'cards',
+        ];
     }
 
     public function registerSettings()
     {
+    }
+
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'Ply',
+            'description' => '',
+            'author' => 'Relenta',
+            'icon' => 'icon-leaf'
+        ];
     }
 }
