7f550e54a58c8c0b3a796c5caa1a534afca0e6c8
Kuroi <denis@golyakov.com>
2017-05-12 13:38:09 +0300

    Basic layout

--- themes/ply/assets/js/app.js
+++ themes/ply/assets/js/app.js
@@ -1,9 +1,24 @@
 require('./common.js');
 
 /*
- * Auto hide navbar
+ * Auto fix header
  */
-jQuery(document).ready(function($){
+$(document).ready(function($){
+	$(".mdl-layout-title").on("click", function() {
+		location.href = "/";
+	});
+/*
+	$("#app").on("scroll", function() {
+		if ($(".mdl-layout__header").offset().top < 1) {
+			$(".mdl-layout__header").addClass("mdl-layout--fixed-header").removeClass("mdl-layout__header--scroll");
+			$(".mdl-layout__container").removeClass("has-scrolling-header");
+		}
+		else {
+			$(".mdl-layout__container").addClass("has-scrolling-header");
+			$(".mdl-layout__header").removeClass("mdl-layout--fixed-header").addClass("mdl-layout__header--scroll");
+		}
+	})
+*/
 });
 
 Vue.component('PlyCard', require('./components/PlyCard.vue'));
