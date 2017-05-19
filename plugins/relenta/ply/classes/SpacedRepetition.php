7ec4eb266ece41d23fb75fcddb7c374a306677d6
Artem Egorov <Kartpick@gmail.com>
2017-05-19 12:53:39 +0300

    * mad fix

--- plugins/relenta/ply/classes/SpacedRepetition.php
+++ plugins/relenta/ply/classes/SpacedRepetition.php
@@ -3,8 +3,17 @@
 use DateTime;
 
 /** Implementation of SM2 */
-class SpacedRepitition {
+class SpacedRepetition {
 
+    /**
+     * Calculates variable, which uses to sort cards descending by this.
+     *
+     * @param string    $dateLastReviewed   Date, card was reviewed last time.
+     * @param float     $daysBetweenReviews How many days should occur between review attempts for this item.
+     * @param float     $performanceRating  Indicator of the correct answer.
+     *
+     * @return float
+     * */
     public static function calcPercentOverdue ($dateLastReviewed, $daysBetweenReviews, $performanceRating) {
         if (static::isCorrect($performanceRating)) {
             $now                = new DateTime();
@@ -17,16 +26,25 @@ class SpacedRepitition {
         return 1;
     }
 
+    /**
+     * @param float $oldDifficulty      Indicator, how difficult card is.
+     * @param float $percentOverdue     Variable, which uses to sort cards descending by this.
+     * @param float $performanceRating  Indicator of the correct answer.
+     *
+     * @return float
+     */
     public static function calcDifficulty ($oldDifficulty, $percentOverdue, $performanceRating) {
         $difficulty = $oldDifficulty + $percentOverdue * 1/17 * (8 - 9 * $performanceRating);
 
         return static::clamp($difficulty);
     }
 
+    /** */
     public static function calcDifficultyWeight ($difficulty) {
         return 3 - 1.7 * $difficulty;
     }
 
+    /** */
     public static function calcDaysBetweenReviews ($oldDaysBetweenReviews, $percentOverdue, $difficultyWeight, $performanceRating) {
         if (static::isCorrect($performanceRating)) {
             return $oldDaysBetweenReviews * (1 + ($difficultyWeight - 1) * $percentOverdue);
@@ -39,6 +57,7 @@ class SpacedRepitition {
         return $oldDaysBetweenReviews * (1 / pow($difficultyWeight, 2));
     }
 
+    /** */
     private static function isCorrect($performanceRating) {
         if ($performanceRating > 0.6) {
             return true;
@@ -46,6 +65,7 @@ class SpacedRepitition {
         return false;
     }
 
+    /** */
     private static function clamp($value, $minValue = 0, $maxValue = 1) {
         return max($minValue, min($maxValue, $value));
     }
